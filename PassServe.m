ee3d7260a2450dc7f8b68d60bdb3f947d082d44c
128keaton <keatonburleson@KPro-2.local>
2013-09-26 17:29:52 -0500

    NEW: Multiple Arguments! :D

--- PassServe.m
+++ PassServe.m
@@ -12,15 +12,15 @@
 
 
 + (void)downloadPassWithURL:(NSURL *)url passName:(NSString *)name webView:(UIWebView *)webView tableView:(UITableView *)tableView overwrite:(BOOL *)overwrite{
-  
+    NSError *error;
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
     AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
- 
+     [[NSFileManager defaultManager] removeItemAtPath: name error: &error];
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSString *path = [[paths objectAtIndex:0] stringByAppendingPathComponent:name];
     operation.outputStream = [NSOutputStream outputStreamToFileAtPath:path append:NO];
  
-    
+
     [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
         NSLog(@"Successfully downloaded file to %@", path);
         NSString *urlAddress = @"http://pass.keatonburleson.com/remove.php";
@@ -33,27 +33,29 @@
         
         //Load the request in the UIWebView.
         [webView loadRequest:requestObj];
+        NSLog(@"Success!");
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         NSLog(@"Error: %@", error);
     }];
     
     [operation start];
     [tableView reloadData];
+
    
 }
 
 + (void)generatePassWithURL:(NSString *)url argument:(NSString *)argument activityView:(UIActivityIndicatorView *)activityIndicator webView:(UIWebView *)webView{
     
-    
+
     [activityIndicator startAnimating];
    
-    NSString *urlAddress = [NSString stringWithFormat:url, argument];
-    
+    NSString *urlAddress = [NSString stringWithFormat:@"%@%@", url, argument];
+    NSLog(urlAddress);
     //Create a URL object.
-    NSURL *url2 = [NSURL URLWithString:urlAddress];
+
     
     //URL Requst Object
-    NSURLRequest *requestObj = [NSURLRequest requestWithURL:url2];
+    NSURLRequest *requestObj = [NSURLRequest requestWithURL:[NSURL URLWithString:urlAddress]];
     
     //Load the request in the UIWebView.
     [webView loadRequest:requestObj];
